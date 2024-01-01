b3339636bdc59c3954b364f0e00545a25aa5938c
kaelx <johnmark.cuyos@evsu.edu.ph>
2024-01-01 17:22:32 +0800

    update

--- index.php
+++ index.php
@@ -134,7 +134,7 @@ if (isset($_POST["login"])) {
         </div>
 
     </main>
-<footer class="footer fixed-bottom d-none d-sm-block bg-light py-3 text-center">
+<footer class="footer fixed-bottom bg-light py-3 text-center">
     <p class="text-muted">Made by: <span class='text-info'>John Mark Cuyos </span> <a href="https://github.com/Kaelx" target="_blank" class="link-warning">(Github Profile)</a></p>
 </footer>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
--- register.php
+++ register.php
@@ -170,6 +170,9 @@ if(isset($_POST["register"])){
         </div>
 
     </main>
+    <footer class="footer fixed-bottom bg-light py-3 text-center">
+    <p class="text-muted">Made by: <span class='text-info'>John Mark Cuyos </span> <a href="https://github.com/Kaelx" target="_blank" class="link-warning">(Github Profile)</a></p>
+</footer>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
 </body>
